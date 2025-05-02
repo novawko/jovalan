9b8fa3b78aa55dac00afc6d5e5b878592d4b4768
Nova Wolfcreek <novawko@gmail.com>
2025-05-02 18:56:12 -0500

    Create petsmasterlist.js

---
+++ styles/js/pages/petsmasterlist.js
@@ -0,0 +1,35 @@
+/* ==================================================================== */
+/* Import Charadex
+======================================================================= */
+import { charadex } from '../charadex.js';
+
+
+/* ==================================================================== */
+/* Load
+======================================================================= */
+document.addEventListener("DOMContentLoaded", async () => {
+
+  let dex = await charadex.initialize.page(
+    null,
+    charadex.page.petsMasterlist,
+    null, 
+    async (listData) => {
+
+      if (listData.type == 'profile') {
+
+        // Create the log dex
+        if (charadex.tools.checkArray(listData.profileArray[0].petslog)) {
+          let logs = await charadex.initialize.page(
+            listData.profileArray[0].petslog,
+            charadex.page.petsMasterlist.relatedData['pets log']
+          );
+        }
+
+      }
+
+    }
+  );
+  
+  charadex.tools.loadPage('.softload', 500);
+  
+});
