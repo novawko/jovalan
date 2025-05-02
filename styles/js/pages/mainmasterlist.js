4bdc0997fb9b2b705361a1018e0426c709774d3c
Nova Wolfcreek <novawko@gmail.com>
2025-05-02 18:56:23 -0500

    Add files via upload

---
+++ styles/js/pages/mainmasterlist.js
@@ -0,0 +1,35 @@
+/* ==================================================================== */
+/* Import Charadex
+======================================================================= */
+import { charadex } from '../charadex.js';
+
+
+/* ==================================================================== */
+/* Load
+======================================================================= */
+document.addEventListener("DOMContentLoaded", async () => {
+
+  let dex = await charadex.initialize.page(
+    null,
+    charadex.page.mainMasterlist,
+    null, 
+    async (listData) => {
+
+      if (listData.type == 'profile') {
+
+        // Create the log dex
+        if (charadex.tools.checkArray(listData.profileArray[0].mainMasterlistlog)) {
+          let logs = await charadex.initialize.page(
+            listData.profileArray[0].mainMasterlistlog,
+            charadex.page.mainMasterlist.relatedData['masterlist log']
+          );
+        }
+
+      }
+
+    }
+  );
+  
+  charadex.tools.loadPage('.softload', 500);
+  
+});
