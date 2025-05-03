336bffad410aa2e66991b770602a74b917bc3863
Nova Wolfcreek <novawko@gmail.com>
2025-05-03 06:30:42 -0500

    Update petsmasterlist.js

--- styles/js/pages/petsmasterlist.js
+++ styles/js/pages/petsmasterlist.js
@@ -19,7 +19,7 @@ document.addEventListener("DOMContentLoaded", async () => {
 
         // Create the log dex
         if (charadex.tools.checkArray(listData.profileArray[0].petslog)) {
-          let petlogs = await charadex.initialize.page(
+          let logs = await charadex.initialize.page(
             listData.profileArray[0].petslog,
             charadex.page.petsMasterlist.relatedData['pets log']
           );
