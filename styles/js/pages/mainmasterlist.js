9c2be4de17541f792408ed17e7c5676df1734adf
Nova Wolfcreek <novawko@gmail.com>
2025-05-03 14:17:35 -0500

    Update mainmasterlist.js

--- styles/js/pages/mainmasterlist.js
+++ styles/js/pages/mainmasterlist.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", async () => {
 
   let dex = await charadex.initialize.page(
     null,
-    charadex.page.mainMasterlist,
+    charadex.page.masterlist,
     null, 
     async (listData) => {
 
@@ -21,7 +21,7 @@ document.addEventListener("DOMContentLoaded", async () => {
         if (charadex.tools.checkArray(listData.profileArray[0].mainmasterlistlog)) {
           let logs = await charadex.initialize.page(
             listData.profileArray[0].mainmasterlistlog,
-            charadex.page.mainmasterlist.relatedData['main masterlist log']
+            charadex.page.masterlist.relatedData['main masterlist log']
           );
         }
 
