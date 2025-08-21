33a298d8ca0d4e6600ec2c66748ccfecfe575c10
Nova Wolfcreek <novawko@gmail.com>
2025-08-20 23:55:46 -0500

    Update species.js

--- styles/js/pages/species.js
+++ styles/js/pages/species.js
@@ -10,7 +10,7 @@ import { charadex } from '../charadex.js';
 document.addEventListener("DOMContentLoaded", async () => {
   let dex = await charadex.initialize.page(null, charadex.page.species, null, 
   (listData) => {
-    let backgroundElement.classList.add = $("cd-prompt-background", "cd-prompt-item-background");
+    let backgroundElement.classList.add("cd-prompt-background", "cd-prompt-item-background");
     if (listData.type == 'profile') {
       backgroundElement.attr('style', `background-image: url(${listData.profileArray[0].image})`);
     } else {
