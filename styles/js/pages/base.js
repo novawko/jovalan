def8b752774cbab7c48ad4a31fabbc52816c3fd3
Nova Wolfcreek <novawko@gmail.com>
2025-08-21 00:31:53 -0500

    Update base.js

--- styles/js/pages/base.js
+++ styles/js/pages/base.js
@@ -26,3 +26,16 @@ document.addEventListener("DOMContentLoaded", async () => {
       }
     }
   });
+
+/* ==================================================================== */
+/* Load any specific navtabs on all of the site
+======================================================================= */
+$(function() {
+  var hash = window.location.hash;
+
+  if (hash != '') {
+    $('.tab-pane').removeClass('active in'); // Remove 'active' from all tab panes
+    $(hash).addClass('active in'); // Add 'active' to the target tab pane
+    $(`a[href="${hash}"]`).tab('show'); // Show the tab linked to the hash
+  }
+});
